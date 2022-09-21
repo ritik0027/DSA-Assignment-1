c1b4ca8c7d9ea3965713d33b7dbc182db3178459
ritik0027 <114002245+ritik0027@users.noreply.github.com>
2022-09-21 15:41:11 +0530

    Create basic data types .cpp

---
+++ basic data types .cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >>e ;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    cout<<setprecision(10)<<d<<endl<<setprecision(10)<<e ;     
+
+    return 0;
+}
